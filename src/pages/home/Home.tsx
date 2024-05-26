8904a908e3d542403956cc616d662592ae0b1324
JensonCHL <j3n50n999@gmail.com>
2024-05-26 15:42:10 +0700

    update

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,71 @@
+
+
+let basicStyle = "flex flex-row flex-shrink-0";
 const Home = () => {
     return (
-        <div className="max-h-screen">Test</div>
+        <div className="bg-white w-[250px] px-10 py-4 m-4 rounded-xl">
+            {/* Add event */}
+            <div className={basicStyle} >
+                <div>ADD NEW</div>
+                <div>
+                    <input type="list" />Event
+                </div>
+            </div>
+
+            {/* Title */}
+            <div className="flex flex-row gap-4" >
+                <div>emote</div>
+                <div>title</div>
+            </div>
+            {/* All day */}
+            <div className="flex flex-row gap-4" >
+                <div>all day</div>
+                <div>togle</div>
+            </div>
+            {/* Starts   */}
+            <div className="flex flex-row gap-4" >
+                <div>starts</div>
+                <div>calender </div>
+                <div>time</div>
+            </div>
+            {/* Ends   */}
+            <div className="flex flex-row gap-4" >
+                <div>ends</div>
+                <div>calender </div>
+                <div>time</div>
+            </div>
+            {/* location */}
+            <div className="flex flex-row gap-4">
+                <div>logo</div>
+                <div>forms</div>
+            </div> 
+            {/* Repeat Reminder */}
+            <div className="flex flex-row gap-4" >
+                <div>repeat</div>
+                <div>reminder</div>
+            </div>
+            {/* Desc */}
+            <div className="flex flex-row gap-4" >
+                add description
+            </div>
+            {/* Checkbox */}
+            <div className="flex flex-row gap-4" >
+                <div>fixed</div>
+                <div>flexible</div>
+            </div>
+            {/* Recomendation */}
+            <div className="flex flex-row gap-4" >
+                <div>Recom</div>
+                <div>check</div>
+                <div>cross</div>
+            </div>
+            {/* Save & cancel */}
+            <div className="flex flex-row gap-4">
+                <div>Save</div>
+                <div>Cancel</div>
+            </div>
+
+        </div>
     ) 
 }
 
