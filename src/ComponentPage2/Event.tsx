6d3172baf3bc4d238f69b0f36bb7ffb34e6c7c4c
JensonCHL <j3n50n999@gmail.com>
2024-06-05 20:58:29 +0700

    update

--- src/ComponentPage2/Event.tsx
+++ src/ComponentPage2/Event.tsx
@@ -2,11 +2,11 @@ const Event = () => {
     return (
         <div className="flex flex-col  w-[30%] h-auto rounded-b-full" >
             {/* header Habit container */}
-            <div className="flex items-center justify-center bg-green-100 rounded-t-[20px] overflow-hidden" >
+            <div className="flex items-center justify-center bg-green-100 rounded-t-[20px] bg-[#E6D7FE] overflow-hidden" >
                 <span className="items-center m-4 text-lg font-bold" >Event</span>
             </div>
             {/* Checkbox */}
-            <div className="flex flex-col h-full gap-3 px-4 py-4 bg-[#FFCCE2] w-full rounded-b-[20px]">
+            <div className="flex flex-col h-full gap-3 px-4 py-4 bg-[#E6D7FE]  w-full rounded-b-[20px]">
                 <div className="flex justify-between ">
                     <div className="flex flex-col" >
                         <span className="font-bold" >Event1</span>
