3ed85a84b6f080df07ace51a0cbf193b0c5c5f3f
JensonCHL <j3n50n999@gmail.com>
2024-06-13 12:33:24 +0700

    repeat

--- src/components/RepeatButton.tsx
+++ src/components/RepeatButton.tsx
@@ -7,8 +7,10 @@ interface Option {
 
 const Dropdown: React.FC = () => {
     const options: Option[] = [
-        { label: 'Repeat', value: 1 },
-        { label: 'Alert', value: 2 },
+        { label: '0', value: 0 },
+        { label: '1', value: 1 },
+        { label: '2', value: 2 },
+        { label: '3', value: 3 },
     ];
     // Repeat event 5 menit sebelum event selesai repeat event 
     const [selectedValues, setSelectedValues] = useState<number[]>([]);
